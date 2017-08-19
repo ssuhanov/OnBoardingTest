39858eb6b75add5e8b9e5176a463e27802878751
Serge Sukhanov <ssuhanov@icloud.com>
2017-08-19 11:21:37 +0300

    Initial commit

---
+++ OnBoardingTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnBoardingTest
+//
+//  Created by Serge Sukhanov on 8/19/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
