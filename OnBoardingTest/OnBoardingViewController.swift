cc7d4cbce129a2e278325a186df054f5dd7adabc
Serge Sukhanov <ssuhanov@icloud.com>
2017-08-19 12:04:56 +0300

    Implement UI and module assembling

--- OnBoardingTest/OnBoardingViewController.swift
+++ OnBoardingTest/OnBoardingViewController.swift
@@ -9,27 +9,19 @@
 import UIKit
 
 class OnBoardingViewController: ViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var presenter: OnBoardingPresenter!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.presenter = OnBoardingPresenter()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func continueAction(_ sender: UIButton) {
+        
     }
-    */
-
+    
 }
