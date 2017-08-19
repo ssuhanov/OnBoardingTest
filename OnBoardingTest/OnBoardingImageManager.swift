04345fd7ccd73cb8b03388609718c4f79d9745f3
Serge Sukhanov <ssuhanov@icloud.com>
2017-08-19 12:32:49 +0300

    Add image manager for OnBoarding

---
+++ OnBoardingTest/OnBoardingImageManager.swift
@@ -0,0 +1,22 @@
+//
+//  OnBoardingImageManager.swift
+//  OnBoardingTest
+//
+//  Created by Serge Sukhanov on 8/19/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+import Foundation
+
+class OnBoardingImageManager {
+    
+    func getImageQueue() -> [String] {
+        // some logic may be here
+        return ["OnBoarding1",
+                "OnBoarding2",
+                "OnBoarding3",
+                "OnBoarding4",
+                "OnBoarding5"]
+    }
+    
+}
