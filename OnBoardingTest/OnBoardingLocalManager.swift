838f271f4baf1c56768f85ed438ca4cc2453a4eb
Serge Sukhanov <ssuhanov@icloud.com>
2017-08-19 12:40:12 +0300

    Add local manager for OnBoarding

---
+++ OnBoardingTest/OnBoardingLocalManager.swift
@@ -0,0 +1,19 @@
+//
+//  OnBoardingLocalManager.swift
+//  OnBoardingTest
+//
+//  Created by Serge Sukhanov on 8/19/17.
+//  Copyright © 2017 Serge Sukhanov. All rights reserved.
+//
+
+import Foundation
+
+class OnBoardingLocalManager {
+    
+    func setFlagOnBoardingCompleted() {
+        // some logic for saving onBoardingCompleted flag
+        // maybe use UserDefaults.standard
+        print("OnBoarding completed succefully")
+    }
+    
+}
